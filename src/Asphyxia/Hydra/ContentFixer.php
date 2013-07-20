16f6a499de76f4252016573c9995201bcd0c97a8
DC* <dc@syncr.com.ar>
2013-07-20 08:07:45 -0300

    Initial commit

---
+++ src/Asphyxia/Hydra/ContentFixer.php
@@ -0,0 +1,36 @@
+<?php
+namespace Asphyxia\Hydra;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+use Symfony\Component\Yaml\Yaml;
+
+
+class ContentFixer implements ServiceProviderInterface
+{
+    protected $arrRegEx;
+    public function __construct($regEx) {
+      $this->arrRegEx = $regEx;
+    }
+
+    public function fix($content) {
+      foreach ($this->arrRegEx as $key => $regEx){
+        if (is_array($regEx)) {
+           list($regEx, $replace) = each($regEx);
+        }else{
+           $replace = '';
+        }
+
+        $content = preg_replace($regEx, $replace, $content);
+      }
+      return $content;
+    }
+
+    public function register(Application $app) {
+       $app['fixer'] = $this;
+    }
+
+    public function boot(Application $app) {
+    }
+}
+?>
